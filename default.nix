1206c85336ba501d038b2d06e480c1f589256cf0
Svein Ove Aas <sveina@gmail.com>
2016-12-18 17:28:19 +0100

    Rename mods.json to definitely.json

--- default.nix
+++ default.nix
@@ -25,7 +25,7 @@ rec {
     # These are all the mods we'd like to include in this pack.
     # (Not yet, they're not.)
     manifests = [
-      ./manifest/mods.json-manifest
+      ./manifest/definitely.json-manifest
     ];
   };
 
